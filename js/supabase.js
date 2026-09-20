22db2d71588ee31d81b114132875ff2d11c5b85d
amjithka1212-bit <amjithka1212@gmail.com>
2026-09-20 10:54:12 +0530

    Fix SUPABASE_URL declaration syntax

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-const SUPABASE_URL = ""https://yhqrwaeidcsqpcimodmf.supabase.co"";
+const SUPABASE_URL = "https://yhqrwaeidcsqpcimodmf.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_GpLtJXUmLlejptThXKz5jg_3_SyqRDo";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
